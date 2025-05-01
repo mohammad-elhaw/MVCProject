8f0a8d9b0c778c5b94adefc0918b556a92906184
Mohammad Elhaw <mohammadelhaw2@gmail.com>
2025-05-01 16:25:10 +0300

    Adding Forget Password feature

---
+++ Project.Presentation/Utilities/Email.cs
@@ -0,0 +1,9 @@
+﻿namespace Project.Presentation.Utilities
+{
+    public class Email
+    {
+        public string To { get; set; } = null!;
+        public string Subject { get; set; } = null!;
+        public string? Body { get; set; }
+    }
+}
